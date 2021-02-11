61f174ffba6795fdaee3f8b2bed38ae43c1fc491
Christopher Kim <chris.kim1990@gmail.com>
2021-02-11 15:46:50 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,3 +1,14 @@
 def find_min_in_nested_arrays(src)
-  
+  array1 = []
+  row_index = 0
+  while row_index < src.count do
+    element_index = 0
+    while element_index < src[row_index].count do
+      if src[row_index][element_index].min do
+        array1 << src[row_index][element_index]
+      end
+      element_index += 1
+    end
+    row_index += 1
+  end
   end
